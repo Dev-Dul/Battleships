d2067780b2c778ce77fc0b9f272dda1cd639c94c
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-01-28 17:45:31 +0100

    All tests for getGrid  passed

---
+++ tests/getGrid.test.js
@@ -0,0 +1,21 @@
+import { GameBoard } from "../src/battleship";
+
+test("Fetches the current grid", () => {
+    let board = new GameBoard();
+    board.place([0, 0], "Destroyer", 4);
+    let testArr = [
+        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    expect(board.getGrid()).toEqual(testArr);
+
+});
